59e41e63d576ae5197493797cc6886701a5c4f17
Rebekah <heal.rebekah@gmail.com>
2021-05-19 13:53:35 +1000

    created overlay toggle to display nav items

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Hair Appointment App
 
 ## Features
-[ ] cards for booking options
-[ ] calendar showing available times for different hairdressers
-[ ] secure transaction
-[ ] invoice card
-[ ] faq page - dropdown cards
-[ ] map with location of shop
-[ ] communicate with admin through chatbox or contact form
+- [ ] cards for booking options
+- [ ] calendar showing available times for different hairdressers
+- [ ] secure transaction
+- [ ] invoice card
+- [ ] faq page - dropdown cards
+- [ ] map with location of shop
+- [ ] communicate with admin through chatbox or contact form
 
 ## TODO
-[x] create react app
-[ ] create burger fold out menu (top right, take up whole page when clicked)
-[ ] create home page
-[ ] services page cards
-[ ] faq dropdown cards
-[ ] booking calendar
-    [ ] retrieve available times from db once date is chosen and populate once date is chosen
-[ ] secure payment
-[ ] invoice card
+- [x] create react app
+- [ ] create overlay fold out menu (top right, take up whole page when clicked)
+- [ ] create home page
+- [ ] services page cards
+- [ ] faq dropdown cards
+- [ ] booking calendar
+    - [ ] retrieve available times from db once date is chosen and populate once date is chosen
+- [ ] secure payment
+- [ ] invoice card
--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import styled from 'styled-components'
 import {Switch, Route, Link, BrowserRouter as Router} from 'react-router-dom'
 import HomePage from './Home/Page'
+import {useState} from 'react'
 
 const NavItemStyle = styled.div``
 
 const NavBarContainer = styled.div`
+    display: ${props => props.overlay === true ? "block" : "none"};
     height: 100vh;
     width: 100vw;
     z-index: 1;
-    background: rgba(0, 0, 0, .8);
+    background: rgba(0, 0, 0, .5);
     overflow: hidden;
     position: fixed;
     top: 0px;
@@ -21,28 +23,40 @@ const NavItem = ({path, title}) =>
         <Link to={path}>{title}</Link>
     </NavItemStyle>
 
-const NavBar = () => 
-    <NavBarContainer>
+// NavItem.propTypes = {
+//     path: PropTypes.string.isRequired,
+//     title: PropTypes.string.isRequired,
+// }
+
+const NavBar = ({overlay}) => 
+    <NavBarContainer overlay={overlay}>
         <NavItem path="/" title="Home"></NavItem>
         <NavItem path="/services" title="Services"></NavItem>
         <NavItem path="/faq" title="FAQ"></NavItem>
     </NavBarContainer>
 
 const NavOverlayContainer = styled.div`
+    z-index: 2;
     position: absolute;
     align-self: flex-end;
-    z-index: 1;
+
     .menu-icon {
         font-size: 30px;
         padding: 28px 35px 0px 0px;
         cursor: pointer;
     }
+    /* background-color: rgba(0,0,0,0.5); Black background with opacity */
 `
 
-const NavOverlay = () => 
-    <NavOverlayContainer>
-        <span className="menu-icon">&#9776;</span>
-    </NavOverlayContainer>
+// const toggleOverlay = (overlay) => setOverlay(!overlay)
+
+// const NavOverlay = ({overlay}) => 
+//     <NavOverlayContainer>
+//         <span className="menu-icon" onClick={() => {
+//             setOverlay(!overlay)
+//             console.log(overlay)
+//         }}>&#9776;</span>
+//     </NavOverlayContainer>
 
 const ContentContainer = styled.div``
 
@@ -60,15 +74,37 @@ const AppContainer = styled.div`
 `
 
 function App() {
+    const [overlay, setOverlay] = useState(false)
+    
     return (
         <AppContainer className="App">
             <Router>
-                <NavOverlay />
-                <NavBar />
+                <NavOverlayContainer>
+                    <span className="menu-icon" onClick={() => {setOverlay(!overlay)}}>&#9776;</span>
+                </NavOverlayContainer>
+                <NavBar overlay={overlay}/>
                 <Content />
             </Router>
         </AppContainer>
     );
 }
+
+// function App() {
+//     return (
+//         <AppContainer className="App">
+//             <Router>
+//                 <div className="menu-btn">
+//                     <span className="menu-icon">&#9776;</span>
+//                 </div>
+//                 <div className="links">
+//                     <Link path="/">First link</Link>
+//                     <Link path="/">First link</Link>
+//                 </div>
+//                 {/* <NavBar /> */}
+//                 <Content />
+//             </Router>
+//         </AppContainer>
+//     );
+// }
 
 export default App;
